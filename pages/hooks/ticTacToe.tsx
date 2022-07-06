edc89ce89120f3aebf1d3fbcaddef83de32b983c
Nhung Luong <nhungluong@MacBook-Pro.local>
2022-07-06 10:31:15 -0700

    TicTacToe component using service

--- pages/hooks/ticTacToe.tsx
+++ pages/hooks/ticTacToe.tsx
@@ -1,214 +1,163 @@
 import type { NextPage } from "next";
-import React, { useContext } from "react";
+import React, { Dispatch, useContext } from "react";
 import styles from "../../styles/Home.module.css";
-import { useInterpret, useMachine } from "@xstate/react";
+import { useMachine, useSelector } from "@xstate/react";
 import {
-  ticTacToeModelContext,
-  TTTContext,
+  ticTacToeMachine, ticTacToeModelContext, TTTContext,
 } from "../../machines/ticTacToeMachine";
-import styled from "styled-components";
-import {
-  BaseActionObject,
-  EventObject,
-  interpret,
-  Interpreter,
-  ResolveTypegenMeta,
-  ServiceMap,
-  TypegenDisabled,
-} from "xstate";
-import { waitFor } from "xstate/lib/waitFor";
-import { useSelector } from "@xstate/react";
+import * as helper from "../../helper/mainFunctions";
 
-export const TicTacToe = () => {
-    const { service } = useContext(TTTContext)
-    // const { send: dispatch } = service
-    console.log(service)
-    // const boardSize = useSelector(service, (state) => {
-    //     return state.context.size;
-    //   });
-    //   console.log(boardSize)
-    // const boardSize = props.context
-  // return (
-  //     <div>in Tic tac toe components</div>
-  // );
-  //   // const [current, send] = useMachine(ticTacToeMachine);
-  //   const service = useContext(TTTContext);
-  //   const [state] = useActor(service.authService);
+const ticTacToe: NextPage = () => {
+  const {service} = useContext(TTTContext);
+  service.start()
 
-  // //   return state.matches("loggedIn") ? "Logged In" : "Logged Out";
-  //   // service.start();
-  //   // console.log(service.state.context);
-  //   //choose size
-  //   const handleSelectSizeBoard = () => {
-  //     if (typeof window !== "undefined") {
-  //       var e = document.getElementById("select1") as HTMLSelectElement;
-  //       var value = parseInt(e.value);
-  //       console.log("size chosen", value);
-  //       service.send("INITIALIZE", { value: value });
-  //     }
-  //   };
+  const currState = useSelector(service, (state) => {return state.value;})
+  const boardSizeSelector = (state: any) => state.context.board.length;
+  const boardSize = useSelector(service, boardSizeSelector);
+  const playerSelector = (state: any) => state.context.player;
+  const player = useSelector(service, playerSelector);
+  const winner = useSelector(service, (state) => {
+    return state.context.winner;
+  });
+  const board = useSelector(service, (state) => {
+    return state.context.board;
+  });
+  const winningLine = useSelector(service, (state) => {
+    return state.context.winning;
+  });
 
-  //   // console.log(size);
-  //   //reset game
-  //   const handleReset = () => {
-  //     console.log("handleReset");
-  //     service.send("RESET");
-  //   };
 
-  //   //render title of game
-  //   function renderTitle() {
-  //     console.log(service.state);
-  //     if (service.state.value == "waiting") {
-  //       return (
-  //         <div>
-  //           <p>
-  //             Choose size board:
-  //             <select id="select1">
-  //               <option >Select an option</option>
-  //               <option value="3">3x3</option>
-  //               <option value="5">5x5</option>
-  //             </select>
-  //             <button onClick={handleSelectSizeBoard}>Done</button>
-  //           </p>
-  //         </div>
-  //       );
-  //     }
-  //     if (service.state.value == "playing") {
-  //       return (
-  //         <p className={styles.subtitle}>
-  //           Your turn: {service.state.context.player}
-  //         </p>
-  //       );
-  //     }
-  //     if (service.state.value == "winner") {
-  //       return (
-  //         <div>
-  //           <p className={styles.subtitle}>
-  //             Congrats! {service.state.context.winner} wins
-  //           </p>
-  //           <p className={styles.smalltitle}>Click reset button to play again</p>
-  //         </div>
-  //       );
-  //     }
-  //     if (service.state.value == "draw") {
-  //       return (
-  //         <div>
-  //           <p className={styles.subtitle}>It's a tie</p>
-  //           <p className={styles.smalltitle}>Click reset button to play again</p>
-  //         </div>
-  //       );
-  //     }
-  //   }
-
-  //   // var x1 = 70.45454406738281;
-  //   // var y1 = 534.5;
-  //   // var x2 = 422.5;
-  //   // var y2 = 816.5;
-
-  //   var x1 = 0;
-  //   var y1 = 0;
-  //   var x2 = 0;
-  //   var y2 = 0;
-
-  //   // function getCoordinates() {
-  //   //   var el = document.getElementById("button");
-  //   //   if (el) {
-  //   //     if (x1 == 0 && y1 == 0 && x2 == 0 && y2 == 0) {
-  //   //       // x1 =
-  //   //       //   (el?.getBoundingClientRect().bottom - el?.getBoundingClientRect().top) *
-  //   //       //   0.5;
-  //   //       x1 = el?.offsetLeft + el?.offsetWidth / 2;
-  //   //       y1 = el?.offsetTop + el?.offsetHeight / 2;
-  //   //       x2 = el?.offsetLeft + el?.offsetWidth / 2;
-  //   //       y2 = el?.offsetTop + el?.offsetHeight / 2;
-  //   //     } else {
-  //   //       x2 = el?.offsetLeft + el?.offsetWidth / 2;
-  //   //       y2 = el?.offsetTop + el?.offsetHeight / 2;
-  //   //     }
-  //   //   }
-  //   //   console.log({ x1, y1 });
-  //   //   console.log({ x2, y2 });
-  //   //   return [x1, x2, y1, y2];
-  //   // }
-
-    //variables in square
-    interface SquareProps {
-      value: number;
-    //   winningLine: boolean;
+  // choose size
+  const handleSelectBoardSize = () => {
+    if (typeof window !== "undefined") {
+      var e = document.getElementById("select1") as HTMLSelectElement;
+      var value = parseInt(e.value);
+      console.log("size chosen", value);
+      service.send("INITIALIZE", { value: value });
     }
+  };
 
-    //create a square with value passed to machine
-    let win = false;
-    function Square(props: SquareProps) {
-      // if having winner => change styling of winning squares
-    //   if (
-    //     props.winningLine &&
-    //     service.state.context.winning.includes(props.value)
-    //   ) {
-    //     // console.log(props.value);
-    //     win = true;
-    //     return (
-    //       <button id="winSquare" className={`${styles.squareHighLight}`}>
-    //         {service.state.context.board[props.value]}
-    //       </button>
-    //     );
-    //   } else {
-        return (
-          <button
-            className={styles.square}
-            id="button"
-            onClick={() => {
-              service.send("PLAY", { value: props.value });
-            }}
-          >
-            {/* {service.state.context.board[props.value]} */}
-            {props.value}
-          </button>
-        );
-    //   }
+  //render title of game
+  function renderTitle() {
+    // console.log(current);
+    if (currState == "waiting") {
+      return (
+        <div>
+          <p className={styles.smalltitle}>
+            I want to play:
+            <select id="select1" className={styles.option}>
+              <option value="0">Select</option>
+              <option value="3">3x3</option>
+              {/* <option value="5">5x5</option> */}
+            </select>
+            <button className={styles.option} onClick={handleSelectBoardSize}>
+              PLAY
+            </button>
+          </p>
+        </div>
+      );
     }
-
-  //function to create n squares
-  function range(start: number, end: number) {
-    return Array(end - start)
-      .fill(null)
-      .map((_, i) => i + start);
+    if (currState == "playing") {
+      return (
+        <p className={styles.subtitle}>Your turn: {player}</p>
+      );
+    }
+    if (currState == "winner") {
+      return (
+        <div>
+          <p className={styles.subtitle}>
+            Congrats! {winner} wins
+          </p>
+          <p className={styles.smalltitle}>Click reset button to play again</p>
+        </div>
+      );
+    }
+    if (currState == "draw") {
+      return (
+        <div>
+          <p className={styles.subtitle}>It's a tie</p>
+          <p className={styles.smalltitle}>Click reset button to play again</p>
+        </div>
+      );
+    }
   }
-  const renderBoard = range(0, 9).map((i) => {
+
+  //variables in square
+  interface SquareProps {
+    value: number;
+    win: boolean;
+  }
+
+  function normalSquare(props: SquareProps) {
     return (
-      <Square
-        key={i}
-        value={i}
-        // winningLine={service.state.context.winner != ""}
-      />
+      <button
+        className={styles.square}
+        id="button"
+        onClick={() => {
+          service.send("PLAY", { value: props.value });
+        }}
+      >
+        {board[props.value]}
+      </button>
+    );
+  }
+
+  //create a square with value passed to machine
+  let win = false;
+  function Square(props: SquareProps) {
+    // check winner
+    if (props.win && winningLine.includes(props.value)) {
+      win = true;
+      return (
+        <button id="winSquare" className={`${styles.squareHighLight}`}>
+          {board[props.value]}
+        </button>
+      );
+    } else {
+      // Player X
+      if (service.state.context.player == "X") {
+        let value = helper.bestMove(service.state);
+        console.log({ value });
+        
+        //update board with index chosen from bestMove();
+        if (props.value == value) {
+          service.send( "PLAY", {value: value });
+          return (
+            <button className={styles.square} id="ai">
+              {board[props.value]}
+            </button>
+          );
+        }
+        return normalSquare(props);
+      }
+      // player O
+      else {
+        return normalSquare(props);
+      }
+    }
+  }
+
+  const renderBoard = helper.range(0, boardSize).map((i) => {
+    console.log("in renderBoard")
+    return (
+      <Square key={i} value={i} win={winner !== ""} />
     );
   });
 
-  //main page
+  // main page
   return (
     <div className={styles.container}>
       <div className={styles.main}>
         <div className={styles.title}>Tic Tac Toe</div>
-        {/* {service.state.context.size == 0 && (
-          <div>
-            <p>
-              Choose size board:
-              <select id="select1">
-                <option value="3">3x3</option>
-                <option value="5">5x5</option>
-              </select>
-              <button onClick={handleSelectSizeBoard}>Done</button>
-            </p>
-          </div>
-        )} */}
-        {/* <div>{renderTitle()}</div> */}
-        <div className={styles.wrapper}>
-          {/* {current.context.winner != "" && (
+        <div>{renderTitle()}</div>
+        {boardSize != 0 && (
+          <div className={styles.wrapper}>
+            {/* {current.context.winner != "" && (
             <div className={styles.svg}>
               <svg
                 className={styles.svg}
                 viewBox="0 0 500 500"
-                preserveAspectRatio="xMidYMid slice"
+                preserveAspectRatio="xMidYMid slice"s
               >
                 <line
                   className={styles.lineHorizontal}
@@ -220,17 +169,22 @@ export const TicTacToe = () => {
               </svg>
             </div>
           )} */}
-
-          <div className={styles.boardGrid}>{renderBoard}</div>
-        </div>
+            <div className={styles.boardGrid}>{renderBoard}</div>
+          </div>
+        )}
 
         <div>
-          {/* <button className={styles.resetButton} onClick={handleReset}> */}
-          {/* Reset
-          </button> */}
+          <button
+            className={styles.resetButton}
+            onClick={() => {
+              service.send("RESET");
+            }}
+          >
+            Reset
+          </button>
         </div>
       </div>
     </div>
   );
 };
-export default TicTacToe;
+export default ticTacToe;
