2eb2d06a93dfe108840a4ec29aa9863dfae7054a
Nhung Luong <nhungluong@MacBook-Pro.local>
2022-07-09 16:13:54 -0700

    add return variables in generateWinningLines function

--- helper/mainFunctions.ts
+++ helper/mainFunctions.ts
@@ -5,14 +5,14 @@ export function range(start: number, end: number) {
         .map((_, i) => i + start);
 }
 
-export function bestMove(board: any) {
+//function to find best next move for computer to win
+export function bestMove(board: any, winningLines: any[][]) {
     let bestScore = -Infinity;
     let move;
-    // let board = current.context.board;
     for (let idx = 0; idx < board.length; idx++) {
         if (board[idx] == "") {
             board[idx] = "X";
-            let score = minimax(board, 0, false);
+            let score = minimax(board, 0, false, winningLines);
             board[idx] = "";
             if (score > bestScore) {
                 bestScore = score;
@@ -23,9 +23,45 @@ export function bestMove(board: any) {
     return move;
 }
 
-export function checkWinner(board: any[]) {
+let scores: Record<string, number> = {
+    "X": 1,
+    "O": -1,
+    "": 0,
+};
+export function minimax(board: any[], depth: number, isMaximizing: boolean, winningLines: any[][]) {
+    let res = checkWinner(board, winningLines);
+    if (res !== null) {
+        return scores[res];
+    }
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let idx = 0; idx < board.length; idx++) {
+            if (board[idx] == "") {
+                board[idx] = "X";
+                let score = minimax(board, depth + 1, false, winningLines);
+                board[idx] = "";
+                bestScore = Math.max(score, bestScore)
+            }
+        }
+        return bestScore;
+    }
+    else {
+        let bestScore = -Infinity;
+        for (let idx = 0; idx < board.length; idx++) {
+            if (board[idx] == "") {
+                board[idx] = "O";
+                let score = minimax(board, depth + 1, true, winningLines);
+                board[idx] = "";
+                bestScore = Math.min(score, bestScore)
+            }
+        }
+        return bestScore;
+    }
+}
+
+//function to check winner if any
+export function checkWinner(board: any[], winningLines: any[][]) {
     let res = "";
-    let winningLines = generateWinningLines(Math.sqrt(board.length))
     var xWon = true;
     for (let line of winningLines) {
         var xWon = true;
@@ -39,7 +75,6 @@ export function checkWinner(board: any[]) {
                 break;
             }
         }
-
         for (let index of line) {
             if (board[index] == "O") {
                 continue;
@@ -55,73 +90,59 @@ export function checkWinner(board: any[]) {
     }
     return res
 }
-let scores: Record<string, number> = {
-    "X": 1,
-    "O": -1,
-    "": 0,
-};
-
-export function minimax(board: any[], depth: number, isMaximizing: boolean) {
-    let res = checkWinner(board);
-    if (res !== null) {
-        return scores[res];
-    }
-    if (isMaximizing) {
-        let bestScore = -Infinity;
-        for (let idx = 0; idx < board.length; idx++) {
-            if (board[idx] == "") {
-                board[idx] = "X";
-                let score = minimax(board, depth + 1, false);
-                board[idx] = "";
-                bestScore = Math.max(score, bestScore)
-            }
-        }
-        return bestScore;
-    }
-    else {
-        let bestScore = -Infinity;
-        for (let idx = 0; idx < board.length; idx++) {
-            if (board[idx] == "") {
-                board[idx] = "O";
-                let score = minimax(board, depth + 1, true);
-                board[idx] = "";
-                bestScore = Math.min(score, bestScore)
-            }
-        }
-        return bestScore;
-    }
-}
 
 export function generateWinningLines(input: number) {
     const array = new Array(input * input).fill(0).map((_, i) => i)
-    var res = [];
-    var lineRow = new Array(input)
+    let res: any[][];
+    let horizontalWin = [];
+    let verticalWin = [];
+    let diagonalWin45 = [];
+    let diagonalWin135 = [];
+    
     for (let i = 0; i < input * input; i += input) {
-      lineRow = array.slice(i, i + input)
-      res.push(lineRow)
+        let lineRow = array.slice(i, i + input)
+        horizontalWin.push(lineRow)
     }
-  
+
     for (let i = 0; i < input; i += 1) {
-      var lineCol = []
-      for (let j = i; j < input * input; j += input) {
-        lineCol.push(j)
-      }
-      res.push(lineCol)
-  
-      if (i == 0) {
-        var lineDiagonal = []
-        for (let k = i; k < input * input; k += (input + 1)) {
-          lineDiagonal.push(k)
+        var lineCol = []
+        for (let j = i; j < input * input; j += input) {
+            lineCol.push(j)
         }
-        res.push(lineDiagonal)
-      }
-      if (i == input - 1) {
-        var lineDiagonal = []
-        for (let k = i; k <= input * (input - 1); k += i) {
-          lineDiagonal.push(k)
+        verticalWin.push(lineCol)
+
+        var lineDiag45 = []
+        if (i == 0) {
+            for (let k = i; k < input * input; k += (input + 1)) {
+                lineDiag45.push(k)
+            }
+            diagonalWin45.push(lineDiag45)
         }
-        res.push(lineDiagonal)
-      }
+
+        var lineDiag135 = []
+        if (i == input - 1) {
+            for (let k = i; k <= input * (input - 1); k += i) {
+                lineDiag135.push(k)
+            }
+            diagonalWin135.push(lineDiag135)
+        }
     }
-    return res;
-  }
+
+    res = [...horizontalWin, ...verticalWin, ...diagonalWin45, ...diagonalWin135]
+    return [res, horizontalWin, verticalWin, diagonalWin45, diagonalWin135];
+}
+
+export function containWin(array2d: number[][], winningLine: number[]) {
+    let result = array2d.filter((item) => {
+        if (item.length === winningLine.length) {
+            for (var i = 0; i < item.length; i++) {
+                if (item[i] !== winningLine[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    });
+    return result.length > 0;
+}
