f2cbe09dfa87a1104c6ebf7047b361be518d0ed0
Nhung Luong <nhungluong@MacBook-Pro.local>
2022-07-12 00:04:51 -0700

    move AI player logic to machine

--- machines/ticTacToeMachine.ts
+++ machines/ticTacToeMachine.ts
@@ -1,7 +1,7 @@
-import { assign, AnyEventObject, Interpreter, EventFrom, ResolveTypegenMeta, TypegenDisabled, BaseActionObject, ServiceMap, createSchema } from "xstate";
+import { AnyEventObject, Interpreter, EventFrom } from "xstate";
 import { createModel } from 'xstate/lib/model';
 import { createContext } from 'react'
-import { generateWinningLines } from "../helper/mainFunctions";
+import { bestMove, checkWinner, generateWinningLines } from "../helper/mainFunctions";
 
 
 //Context
@@ -12,6 +12,7 @@ export const ticTacToeModelContext = {
   player: "X",
   winner: "",
   winning: Array(),
+  winningLines: Array(),
 };
 export interface ITTTContext {
   service: Interpreter<
@@ -25,154 +26,149 @@ export const TTTContext = createContext({} as ITTTContext)
 
 //Events
 export const ticTacToeModelEvents = {
-  // events: {} as TTTEvents
   events: {
     '': () => ({}), // empty
-    INITIALIZE: (value: number) => ({ value }),
-    PLAY: (index: number) => ({ index }),
+    INITIALIZE: () => ({}),
+    PLAY: () => ({}),
+    FINDMOVE: () => ({}),
     winner: () => ({}),
     RESET: () => ({}),
   }
 };
 
-// export type TTTEvents =
-//   | { type: "INITIALIZE"; value: number }
-//   | { type: "RESET" }
-//   | { type: "PLAY"; index: number }
-//   | { type: "winner" }
-
-
 
 export const ticTacToeModel = createModel(ticTacToeModelContext, ticTacToeModelEvents);
-// export const ticTacToeModel = createModel(ticTacToeModelContext);
-export const ticTacToeMachine = ticTacToeModel.createMachine(
-  {
-    id: "tictactoe",
-    schema: {
-      context: {} as typeof ticTacToeModelContext,
-      // events: {} as UnionFromCreatorsReturnTypes<FinalEventCreators<typeof ticTacToeModelEvents>>,
-    },
-    preserveActionOrder: true,
-    context: ticTacToeModel.initialContext,
-    initial: "waiting",
-    states: {
-      waiting: {
-        on: {
-          INITIALIZE: { target: "playing", actions: ["assignSize", "initialBoard"] }
-        }
-      },
-      playing: {
-        "always": [
-          { target: "winner", cond: "checkWin" },
-          { target: "draw", cond: "checkDraw" }
-        ],
-        on: {
-          PLAY: [
+export const ticTacToeMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcCWBjZBDTB7MAdAO5apoB2UAxAJIByNAKjQIIAyNAWgKKKgAOuWGVS5yfEAA9EAWgCMAZgDsBAKwAWVQAYAHOp06ATKrk6AnABoQAT1lz1ANgJnDS+w7frD6s2YC+flZomDjI+AT8ADZY1qiUVAAKbCwAmhKCwmhiEtIIMqoE+kraZqpmWkrF6loKVrZ5roYECq5KhmYKZQ7qCmZKAUEY2HiEUTFx1OlCItlIUohyZuoEDjpySjpaDlrqjg6GdXYOBb1rcg6mfabHAyDBw2FgVABK3ADK3IxTmaLic7kydRyJo6JQ1XYVLSGOQaQ55RRNDo+JSrBS9OQVHQBQIgci4CBwCT3ULhEgiSjfGZ-UAA1xOYyqSqgmpbNEOOHyRaFVQtQzdByrFw6By3YkjCLRWIUuYZKk5RDGAhQlmaHZmfYKIwcuQY7mbKHaEzaLaioYkwhEOLkMAAJ0pWWp8wQPQUBGhOx5PK0qkZ6m1POc0MZ210CgqClNIXFEBtWCI9t+8ry1R0hX2mxaDk6Sj9NlkCicWiLCJaS1MihFOLFjwTsxpsh6TmZCghYKDufqMmzbt84cUWgxFXU2L8QA */
+  ticTacToeModel.createMachine(
+    {
+      context: ticTacToeModel.initialContext,
+      schema: { context: {} as typeof ticTacToeModelContext },
+      preserveActionOrder: true,
+      id: "tictactoe",
+      initial: "waiting",
+      states: {
+        waiting: {
+          on: {
+            INITIALIZE: {
+              actions: ["assignSize", "initialBoard"],
+              target: "XTurn",
+            },
+          },
+        },
+        XTurn: {
+          always: [
             {
-              target: "playing",
-              cond: "isValidMove",
+              cond: "checkWin",
+              target: "winner",
+            },
+            {
+              cond: "checkDraw",
+              target: "draw",
+            },
+            {
+              target: "OTurn",
               actions: "updateBoard",
-            }
+            },
           ]
-        }
+        },
+        OTurn: {
+          always: [
+            {
+              cond: "checkWin",
+              target: "winner",
+            },
+            {
+              cond: "checkDraw",
+              target: "draw",
+            },
+          ],
+          on: {
+            PLAY: {
+              actions: "updateBoard",
+              cond: "isValidMove",
+              target: "XTurn",
+            },
+          },
+        },
+        winner: {
+          entry: ["setWinner", "setWinningLine"],
+        },
+        draw: {},
       },
-      winner: {
-        entry: ["setWinner", "setWinningLine"]
+      on: {
+        RESET: {
+          actions: "resetGame",
+          target: "waiting",
+          internal: false,
+        },
       },
-      draw: {}
     },
-    on: {
-      RESET: { target: "waiting", actions: "resetGame" }
-    }
-  },
-  {
-    actions: {
-      assignSize: ticTacToeModel.assign({
-        size: (ctx, event: AnyEventObject) => {
-          console.log("in update size board", { event });
-          return ctx.size = event.value
-        },
-      }),
-      initialBoard: ticTacToeModel.assign({
-        board: (ctx) => {
-          console.log("in initialize board");
-          let initialBoard = Array(ctx.size * ctx.size).fill("");
-          return ctx.board = initialBoard;
-        },
-        winning: (ctx) => {
-          console.log(ctx);
-          return ctx.winning = Array(ctx.size).fill("");
-        },
-      }),
-      updateBoard: ticTacToeModel.assign({
-        board: (context, event: AnyEventObject) => {
-          const updatedBoard = [...context.board];
-          console.log("in update board", event.value);
-          updatedBoard[event.value] = context.player;
-          return updatedBoard;
-        },
-        moves: (context) => context.moves + 1,
-        player: (context) => (context.player === "X" ? "O" : "X")
-      }),
-      resetGame: ticTacToeModel.assign(ticTacToeModel.initialContext),
-      setWinner: ticTacToeModel.assign({
-        winner: (ctx) => (
-          ctx.player === "X" ? "O" : "X"
-        ),
-      }),
-      setWinningLine: ticTacToeModel.assign({
-        winner: (ctx) => (
-          ctx.player === "X" ? "O" : "X"
-        ),
-      })
-    },
-    guards: {
-      checkWin: (ctx) => {
-        // console.log("in check winner")
-        //all possible lines to win
-        const winningLines = generateWinningLines(Math.sqrt(ctx.board.length))[0];
-        // console.log({winningLines})
-        var xWon = true;
-        for (let line of winningLines) {
-          var xWon = true;
-          var oWon = true;
-          for (let index of line) {
-            if (ctx.board[index] == "X") {
-              continue;
+    {
+      actions: {
+        assignSize: ticTacToeModel.assign({
+          size: (ctx, event: AnyEventObject) => {
+            console.log("in update size board", { event });
+            return ctx.size = event.value
+          },
+        }),
+        initialBoard: ticTacToeModel.assign({
+          board: (ctx) => {
+            console.log("in initialize board");
+            let initialBoard = Array(ctx.size * ctx.size).fill("");
+            return ctx.board = initialBoard;
+          },
+          winning: (ctx) => {
+            return ctx.winning = Array(ctx.size).fill("");
+          },
+          winningLines: (ctx) => {
+            return ctx.winningLines = generateWinningLines(ctx.size)[0]
+          }
+        }),
+        updateBoard: ticTacToeModel.assign({
+          board: (ctx, event: AnyEventObject) => {
+            const updatedBoard = [...ctx.board];
+            if (ctx.player == "O") {
+              updatedBoard[event.value] = ctx.player;
             }
             else {
-              xWon = false;
-              break;
+              console.log("FIND MOVE FOR X")
+              let idx = bestMove(ctx.board, ctx.winningLines);
+              if (idx != null && updatedBoard[idx] == '') {
+                updatedBoard[idx] = ctx.player;
+              }
             }
+            return updatedBoard;
+          },
+          moves: (ctx) => ctx.moves + 1,
+          player: (ctx) => (ctx.player === "X" ? "O" : "X")
+        }),
+        resetGame: ticTacToeModel.assign(ticTacToeModel.initialContext),
+        setWinner: ticTacToeModel.assign({
+          winner: (ctx) => (
+            ctx.player === "X" ? "O" : "X"
+          ),
+        }),
+        setWinningLine: ticTacToeModel.assign({
+          winning: (ctx) => {
+            var res = checkWinner(ctx.board, ctx.winningLines);
+            return ctx.winning = res.winningLine
           }
-
-          for (let index of line) {
-            if (ctx.board[index] == "O") {
-              continue;
-            }
-            else {
-              oWon = false;
-              break;
-            }
-          }
-
-          if (xWon || oWon) {
-            ctx.winning = line;
-            console.log("winning", ctx.winning)
-            return true;
-          }
-        }
-        return false;
+        })
       },
-      checkDraw: (ctx) => {
-        // return false
-        // console.log("in check draw", ctx.board.length)
-        return ctx.moves === ctx.board.length;
-      },
-      isValidMove: (ctx, event: AnyEventObject) => {
-        // console.log("check valid move");
-        return ctx.board[event.value] === "";
-      },
-    }
-  },
-);
+      guards: {
+        checkWin: (ctx) => {
+          console.log("check win")
+          return checkWinner(ctx.board, ctx.winningLines).winner != "";
+        },
+        checkDraw: (ctx) => {
+          console.log("check draw")
+          return ctx.moves === ctx.board.length;
+        },
+        isValidMove: (ctx, event: AnyEventObject) => {
+          console.log("check valid move")
+          return ctx.board[event.value] === "";
+        },
+      }
+    },
+  );
