4722b48a148d951fc0a93943471eac4e0f959ab7
Nhung Luong <nhungluong@MacBook-Pro.local>
2022-07-18 15:45:20 -0700

    add twitter contact

--- component/footer/footer.tsx
+++ component/footer/footer.tsx
@@ -25,11 +25,61 @@ const StyledFooter = styled.footer<{}>`
     margin: 0;
   }
   .link {
+    align-items: center;
     margin: 0 1rem;
     display: flex;
     flex-direction: column;
   }
+
 `;
+
+const StyledContactInfo = styled.span<{id: string}>`
+  width: 42px;
+  height: 42px;
+  cursor: pointer;
+  -webkit-transition-property: all;
+  -webkit-transition-duration: 0.3s;
+  -webkit-transition-timing-function: ease;
+
+  :hover {
+    transform: scale(1.5);
+  }
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  ${(props) => {
+    var id = props.id;
+    if (id == "LinkedIn") return `background-image: url("In-Blue-21@2x.png");`
+    if (id == "Github") return `background-image: url("GitHub-Mark-64px.png");`
+    if (id == "Email") return `background-image: url("gmail-icon-38472.png");`
+    if (id == "Twitter") return `background-image: url("Twitter-logo-blue.png"); `
+  }};
+`; 
+
+const ContactInfo = (props: {id: string}) => {
+  var href = "";
+  var id = props.id;
+  if (id == "LinkedIn") {
+    href = "https://www.linkedin.com/in/nhungluong/";
+  }
+  if (id == "Github") {
+    href = "https://github.com/nhungL";
+  }
+  if (id == "Email") {
+    href = "mailto:nhung.luong1098@gmail.com";
+  }
+  if (id == "Twitter") {
+    href = "https://twitter.com/NhungLuong12";
+  }
+
+  return (
+    <a className="link" href={href}>
+      <p className="p">{id}</p>
+      <StyledContactInfo id={id}/>
+    </a>
+  );
+};
+
 export const Footer = () => {
   return (
     <StyledFooter>
@@ -38,39 +88,11 @@ export const Footer = () => {
       </div>
       <div className="contact">
         <ul className="list">
-          <a className="link" href="https://www.linkedin.com/in/nhungluong/">
-            <p className="p">LinkedIn</p>
-            <span>
-              <Image
-                className="icon"
-                src="/In-Blue-21@2x.png"
-                height="40rem"
-                width="40rem"
-              ></Image>
-            </span>
-          </a>
-          <a className="link" href="https://github.com/nhungL">
-            <p className="p">Github</p>
-            <span>
-              <Image
-                className="icon"
-                src="/GitHub-Mark-64px.png"
-                height="40rem"
-                width="40rem"
-              ></Image>
-            </span>
-          </a>
-          <a className="link" href="mailto:nhung.luong1098@gmail.com">
-            <p className="p">Email</p>
-            <span>
-              <Image
-                className="icon"
-                src="/gmail-icon-38472.png"
-                height="40rem"
-                width="40rem"
-              ></Image>
-            </span>
-          </a>
+          <ContactInfo id="LinkedIn"/>
+          <ContactInfo id="Github"/>
+          <ContactInfo id="Twitter"/>
+          <ContactInfo id="Email"/>
+          
         </ul>
       </div>
     </StyledFooter>
