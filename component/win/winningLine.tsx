bdbcfc51f214ed74896b7b1c4bd99a071b7ebc1e
Nhung Luong <nhungluong@MacBook-Pro.local>
2022-07-19 18:39:41 -0700

    fix winning index

--- component/win/winningLine.tsx
+++ component/win/winningLine.tsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 import { useContext } from "react";
 import { TTTContext } from "../../machines/ticTacToeMachine";
 import { useSelector } from "@xstate/react";
+import { checkWinner } from "../../helper/mainFunctions";
 
 const Rotate = styled.hr<{ id: string }>`
   color: #680000;
   border-radius: 10rem;
   box-shadow: 0 0 0 2px #680000;
-  // border-top: 0.3rem solid;
   position: absolute;
   margin: 0;
   ${(props) => {
@@ -24,20 +24,26 @@ export const WinningLine = () => {
   const winningLine = useSelector(service, (state) => {
     return state.context.winning;
   });
+  const board = useSelector(service, (state) => {
+    return state.context.board;
+  });
   const boardSizeSelector = (state: any) => state.context.size;
   const boardSize = useSelector(service, boardSizeSelector);
-  let [res, horizontalWin, verticalWin, diagonalWin45, diagonalWin135] = generateWinningLines(boardSize);
-  if (containWin(horizontalWin, winningLine)) {
+  let [res, horizontalWin, verticalWin, diagonalWin45, diagonalWin135] =
+    generateWinningLines(boardSize);
+  if (boardSize == 3) {
+    if (containWin(horizontalWin, winningLine)) {
+      return <Rotate id="0" />;
+    } else if (containWin(verticalWin, winningLine)) {
+      return <Rotate id="90" />;
+    } else if (containWin(diagonalWin45, winningLine)) {
+      return <Rotate id="45" />;
+    } else if (containWin(diagonalWin135, winningLine)) {
+      return <Rotate id="135" />;
+    } else return <Rotate id="0" />;
+  } else { 
+    //5x5
+    console.log({horizontalWin, verticalWin, diagonalWin45, diagonalWin135});
     return <Rotate id="0" />;
   }
-  if (containWin(verticalWin, winningLine)) {
-    return <Rotate id="90" />;
-  }
-  if (containWin(diagonalWin45, winningLine)) {
-    return <Rotate id="45" />;
-  }
-  if (containWin(diagonalWin135, winningLine)) {
-    return <Rotate id="135" />;
-  }
-  return <Rotate id="0" />;
 };
